ac05f47c54da59c71d1e2dab69e29dd4af2e31ba
dngda <opowaekeno123@gmail.com>
2019-10-04 21:30:08 +0700

    Initial Commit

---
+++ app/src/main/java/com/airmineral/newkotlinapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.airmineral.newkotlinapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private var number = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textV.text = getString(R.string.welcomeText, number)
+        btnTap.setOnTouchListener(RepeatListener(500, 10, View.OnClickListener {
+            nambah()
+        }))
+    }
+
+    private fun nambah() {
+        number++
+        textV.text = getString(R.string.welcomeText, number)
+    }
+}
